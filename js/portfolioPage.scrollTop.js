9c66bc827665b6cc93950230acafb2d3f534ede8
fots <60556553+fotscode@users.noreply.github.com>
2021-01-11 17:44:19 -0300

    initial commit

---
+++ js/portfolioPage.scrollTop.js
@@ -0,0 +1,18 @@
+mybutton = document.getElementById("scrollTop");
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "block";
+		mybutton.style.zIndex="2";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+function topFunction() {
+  document.body.scrollTop = 0; // For Safari
+  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+}
